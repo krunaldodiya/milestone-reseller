e6f56ce082e01a5d1e9252b7669942cb0e789610
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-19 15:25:28 +0530

    added institute login

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -1,8 +1,17 @@
 import React, {Fragment} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {useStoreState} from 'easy-peasy';
+import {Button} from 'react-native-elements';
 
 export const UserInfo = (props: any) => {
   const {user} = props.navigation.state.params;
+  const institute = useStoreState(state => state.institute);
+
+  const checkSubscription = (category_id: any) => {
+    return !!user.subscriptions.filter((subscription: any) => {
+      return subscription.category_id === category_id;
+    }).length;
+  };
 
   return (
     <Fragment>
@@ -10,8 +19,76 @@ export const UserInfo = (props: any) => {
 
       <SafeAreaView style={{flex: 1}}>
         <View style={styles.container}>
-          <Text>{user.name}</Text>
-          <Text>{user.mobile}</Text>
+          <View style={{padding: 5}}>
+            <Text style={{fontSize: 18, marginBottom: 7}}>{user.name}</Text>
+            <Text style={{fontSize: 14, marginBottom: 0}}>{user.mobile}</Text>
+          </View>
+
+          <View style={{marginTop: 10}}>
+            <View
+              style={{
+                padding: 5,
+                paddingBottom: 10,
+                borderBottomWidth: 1,
+                borderBottomColor: '#bbb',
+              }}>
+              <Text style={{fontSize: 18, textTransform: 'uppercase'}}>
+                Subscriptions
+              </Text>
+            </View>
+
+            <View style={{padding: 5}}>
+              {institute.categories.map((category: any) => {
+                const isSubscribed = checkSubscription(category.category_id);
+
+                return (
+                  <View
+                    style={{
+                      marginTop: 5,
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                    }}
+                    key={category.id}>
+                    <View>
+                      <Text style={{color: 'black', fontSize: 16}}>
+                        {category.info.name}
+                      </Text>
+                    </View>
+
+                    <View>
+                      {isSubscribed ? (
+                        <Button
+                          buttonStyle={{
+                            backgroundColor: 'red',
+                            width: 100,
+                            height: 30,
+                          }}
+                          titleStyle={{
+                            fontSize: 12,
+                            textTransform: 'uppercase',
+                          }}
+                          title="unsubscribe"
+                        />
+                      ) : (
+                        <Button
+                          buttonStyle={{
+                            backgroundColor: 'green',
+                            width: 100,
+                            height: 30,
+                          }}
+                          titleStyle={{
+                            fontSize: 12,
+                            textTransform: 'uppercase',
+                          }}
+                          title="subscribe"
+                        />
+                      )}
+                    </View>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </Fragment>
